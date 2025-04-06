f72e85de92cfdbce860cb5b2a279de19939f7c6e
Brenda Mejia <mejiab@protonmail.com>
2025-04-05 22:20:55 -0400

    add dependencies

--- api/analyze_image/function_image_analyzer.py
+++ api/analyze_image/function_image_analyzer.py
@@ -1,7 +1,22 @@
 import azure.functions as func
 import logging
+import os
+from azure.storage.blob import BlobServiceClient
 
 def main(myblob: func.InputStream):
     logging.info(f"Processing file: {myblob.name}")
+
+        # Retrieve the connection string from environment variables
+    connection_string = os.getenv('AzureWebJobsStorage')
+    
+    # Create BlobServiceClient using the connection string
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    
+    # Example: List containers in the storage account
+    containers = blob_service_client.list_containers()
+    for container in containers:
+        logging.info(f"Container found: {container['name']}")
+    
+    # Your image processing logic here
 
     # image processing
